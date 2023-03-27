9d51922750ac62b4e60d59f3a340a8395485a9ab
haru-ki-n <ruriharuki@gmail.com>
2023-03-28 05:08:39 +0900

    削除確認ダイアログ作成

--- student_list_system_front/src/components/UserIndex.vue
+++ student_list_system_front/src/components/UserIndex.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="overflow">
         <div class="mx-auto h1" style="text-align:center; margin-top:20px;margin-bottom:20px;">
             研修生一覧
         </div>
@@ -22,7 +22,10 @@
                 <td>{{ user.name }}</td>
                 <td>{{ user.mailAddress }}</td>
                 <td>{{ user.image }}</td>
-                <td><img src="/icon/trashbox.png" alt="削除" v-on-click="check(user.id, user.name, user.mailAddress)"></td>
+                <td>
+                    <img src="/icon/trashbox.png" alt="削除"
+                        v-on:click="check({ id: user.id, name: user.name, mailAddress: user.mailAddress })">
+                </td>
                 <td><img src="/icon/pencil.png" alt="編集"></td>
             </tr>
         </table>
@@ -47,89 +50,88 @@ export default {
                 console.log(error);
             });
     },
-    check(id, name, mailAddress) {
-        const overflowTargetElements = document.getElementById("overflow");
-        const overflow = `
-        <div id="conf" style="text-align:center; z-index: 10;">
-            <p>ID:<block id="userId"></block></p>
-            <p>名前:<block id="userName"></block></p>
-            <p>メールアドレス:<block id="userMailAddress"></block></p>
-            <p>こちらのデータを削除します。本当によろしいですか?</p>
-            <div class="btns" style="text-align:center">
-                <input type="button" class="btn btn-danger rounded-pill btn-lg" value="削除" onclick="next(${id});">
-                <input type="button" class="btn btn-secondary rounded-pill btn-lg" value="キャンセル" onclick="cancel();">
-            </div>
-        </div>`;
+    methods: {
+        check(user) {
+            const overflowTargetElements = document.getElementById("overflow");
+            const overflow = `
+                <div id="conf" style="text-align:center; z-index: 10;">
+                    <p>ID:<block id="userId"></block></p>
+                    <p>名前:<block id="userName"></block></p>
+                    <p>メールアドレス:<block id="userMailAddress"></block></p>
+                    <p>こちらのデータを削除します。本当によろしいですか?</p>
+                    <div class="btns" style="text-align:center">
+                        <button type="button" class="btn btn-danger rounded-pill btn-lg" v-on:click="next()">削除</button>
+                        <button type="button" class="btn btn-secondary rounded-pill btn-lg" v-on:click="cancel()">キャンセル</button>
+                    </div>
+                </div>`;
 
-        overflowTargetElements.insertAdjacentHTML('afterbegin', overflow);
-        overflowTargetElements.style.zIndex = "10";
+            overflowTargetElements.insertAdjacentHTML('afterbegin', overflow);
+            overflowTargetElements.style.zIndex = "10";
 
-        const overflowCss = document.querySelectorAll("#overflow");
-        overflowCss.forEach(el => {
-            Object.assign(el.style, {
-                width: "100vw",
-                height: "100vh",
-                backgroundColor: "rgba(0, 0, 0, 0.2)",
-                top: "0%",
-                left: "0%",
-                position: "absolute",
-                display: "none",
+            const overflowCss = document.querySelectorAll("#overflow");
+            overflowCss.forEach(el => {
+                Object.assign(el.style, {
+                    width: "100vw",
+                    height: "100vh",
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    top: "0%",
+                    left: "0%",
+                    position: "absolute",
+                    display: "none",
+                });
             });
-        });
 
-        const confCss = document.getElementById("conf");
-        Object.assign(confCss.style, {
-            zIndex: "10",
-            background: "#FFF",
-            padding: "20px",
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-        });
+            const confCss = document.getElementById("conf");
+            Object.assign(confCss.style, {
+                zIndex: "10",
+                background: "#FFF",
+                padding: "20px",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+            });
 
-        const userId = document.getElementById("userId");
-        const userName = document.getElementById("userName");
-        const userMailAddress = document.getElementById("userMailAddress");
+            const userId = document.getElementById("userId");
+            const userName = document.getElementById("userName");
+            const userMailAddress = document.getElementById("userMailAddress");
 
-        userId.textContent = id;
-        userName.textContent = name;
-        userMailAddress.textContent = mailAddress;
+            userId.textContent = user.id;
+            userName.textContent = user.name;
+            userMailAddress.textContent = user.mailAddress;
 
-        $("#overflow").show();
-    },
-    next() {
-        let id = document.getElementById("userId").innerText;
-        let request = new XMLHttpRequest();
-        request.open("post", "/delete_complete", true);
-        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        request.onreadystatechange = function () {
-            if (request.readyState === 1) {
-                console.log("openメソッドが呼ばれました");
-            } else if (request.readyState === 2) {
-                console.log("sendメソッドが呼ばれました");
-            } else if (request.readyState === 3) {
-                console.log("レスポンスの受信中");
-            }
-            else if (request.readyState === 4 && request.status === 200) {
-                if (!request.responseText) {
-                    alert("該当するデータはありませんでした");
-                    return;
+            $("#overflow").show();
+        },
+        next() {
+            let id = document.getElementById("userId").innerText;
+            let request = new XMLHttpRequest();
+            request.open("post", "/delete_complete", true);
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.onreadystatechange = function () {
+                if (request.readyState === 1) {
+                    console.log("openメソッドが呼ばれました");
+                } else if (request.readyState === 2) {
+                    console.log("sendメソッドが呼ばれました");
+                } else if (request.readyState === 3) {
+                    console.log("レスポンスの受信中");
                 }
-                // var targetElement = document.getElementById('usersList');
-                // targetElement.innerHTML = request.responseText;
-                location.reload(true);
-                $("#conf").remove();
-                $("#overflow").hide();
-                request.send(null);
-            }
-        };
-        request.send("id=" + encodeURIComponent(id));
-    },
-
-    cancel() {
-        $("#conf").remove();
-        $("#overflow").hide();
+                else if (request.readyState === 4 && request.status === 200) {
+                    if (!request.responseText) {
+                        alert("該当するデータはありませんでした");
+                        return;
+                    }
+                    $("#conf").remove();
+                    $("#overflow").hide();
+                    request.send(null);
+                    location.reload(true);
+                }
+            };
+            request.send("id=" + encodeURIComponent(id));
+        },
+        cancel() {
+            $("#conf").remove();
+            $("#overflow").hide();
+        }
     }
 };
 </script>
