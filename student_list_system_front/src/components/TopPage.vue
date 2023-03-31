081586925e78644941e12330b70d2e82309b848e
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-03-31 13:10:07 +0900

    追加課題作成中

--- student_list_system_front/src/components/TopPage.vue
+++ student_list_system_front/src/components/TopPage.vue
@@ -1,10 +1,32 @@
 <template>
     <div style="text-align:center">
         <h1>研修生コミュニティ</h1>
-        <b-button pill variant="primary" size="lg" router-link to="/userIndex">一覧</b-button>
+        <form @submit.prevent="">
+            <label>User ID</label>
+            <input type="text" placeholder="customer id" v-model="user.id"/>
+            <br>
+            <label>Password</label>
+            <input type="password" placeholder="password" v-model="user.password"/>
+            <br>
+            <b-button type="submit" pill variant="primary" size="lg">ログイン</b-button>
+        </form>
     </div>
 </template>
-
-<style>
-
-</style>
+<script>
+export default {
+    data() {
+        return {
+            user: {}
+        };
+    },
+    methods: {
+        doLogin() {
+            this.$store.dispatch("auth", {
+                userId: this.user.userId,
+                userToken: 'dummy token'
+            });
+            this.$router.push(this.$route.query.redirect);
+        }
+    }
+};
+</script>
